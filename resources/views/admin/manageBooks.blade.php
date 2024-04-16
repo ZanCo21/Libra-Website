11e75d0694c7edd5bcfc7a3e495f5118116d2e47
ZanCo21 <fauzan.alghifari21@gmail.com>
2024-04-16 14:38:42 +0700

    feat: show data in manageBooks

--- resources/views/admin/manageBooks.blade.php
+++ resources/views/admin/manageBooks.blade.php
@@ -45,59 +45,52 @@
                             </tr>
                         </thead>
                         <tbody class="table-border-bottom-0">
-                            <tr>
-                                <td>
-                                    <i class="fab fa-angular fa-lg text-danger me-3"></i>
-                                    <span class="fw-medium">Angular Project</span>
-                                </td>
-                                <td>
-                                    <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                            class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                                            <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle lazyload" />
-                                        </li>
-                                        <li>
-                                            Albert Cook
-                                        </li>
-                                    </ul>
-                                </td>
-                                <td>
-                                    <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                            class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                                            <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle lazyload" />
-                                        </li>
-                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                            class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                                            <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle lazyload" />
-                                        </li>
-                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                            class="avatar avatar-xs pull-up" title="Christina Parker">
-                                            <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle lazyload" />
-                                        </li>
-                                    </ul>
-                                </td>
-                                <td><span class="badge bg-label-primary me-1">Active</span></td>
-                                <td>
-                                    <div class="dropdown">
-                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                            data-bs-toggle="dropdown">
+                            @foreach ($books as $item)
+                                <tr>
+                                    <td>
+                                        <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                        <span class="fw-medium">{{ $item->judul }}</span>
+                                    </td>
+                                    <td>
+                                        <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                                class="avatar avatar-xs pull-up" title="Lilian Fuller">
+                                                <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle lazyload" />
+                                            </li>
+                                            <li>
+                                                {{ $item->penulis }}
+                                            </li>
+                                        </ul>
+                                    </td>
+                                    <td>
+                                        <span class="fw-medium formatDate">{{ $item->tahunTerbit }}</span>
+                                    </td>
+                                    @if ($item->stock == 0)
+                                    <td><span class="badge bg-label-primary me-1">out of stock</span></td>
+                                    @else
+                                    <td><span class="badge bg-label-primary me-1">Available</span></td>
+                                    @endif
+                                    <td>
+                                        <div class="dropdown">
+                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                                data-bs-toggle="dropdown">
+                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item" href="javascript:void(0);"><i
+                                                        class="bx bx-edit-alt me-1"></i> Edit</a>
+                                                <a class="dropdown-item" href="javascript:void(0);"><i
+                                                        class="bx bx-trash me-1"></i> Delete</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-icon btn-primary">
                                             <i class="bx bx-dots-vertical-rounded"></i>
                                         </button>
-                                        <div class="dropdown-menu">
-                                            <a class="dropdown-item" href="javascript:void(0);"><i
-                                                    class="bx bx-edit-alt me-1"></i> Edit</a>
-                                            <a class="dropdown-item" href="javascript:void(0);"><i
-                                                    class="bx bx-trash me-1"></i> Delete</a>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-icon btn-primary">
-                                        <span class="tf-icons bx bx-pie-chart-alt"></span>
-                                    </button>
-                                </td>
-                            </tr>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -266,5 +259,27 @@
                 },
             });
         });
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var dateCells = document.querySelectorAll('.formatDate');
+            dateCells.forEach(function(cell) {
+                var originalDate = cell.textContent.trim();
+                var formattedDate = new Date(originalDate).toLocaleDateString('en-US', {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric'
+                });
+                cell.textContent = formattedDate;
+            });
+        });
+
+        function formatDateString(dateString) {
+            var dateObj = new Date(dateString);
+            return dateObj.toLocaleDateString('en-US', {
+                day: 'numeric',
+                month: 'short',
+                year: 'numeric'
+            });
+        }
     </script>
 @endsection
